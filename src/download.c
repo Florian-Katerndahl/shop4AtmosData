dd2870916b945e1559d9c2800482dbd8ff6931b2
Florian-Katerndahl <florian@katerndahl.com>
2023-02-12 22:49:52 +0100

    update library files

--- src/download.c
+++ src/download.c
@@ -1,11 +1,63 @@
-//
-// Created by florian on 08.02.23.
-//
 #include <stdio.h>
+#include <stdlib.h>
 #include "download.h"
 
 void print_usage(void) {
-    printf("Usage: cams-download <args>\n\n"
-           "-h\t\n"
-           "-v\t\n");
+    printf(
+        "Usage: cams-download [-h|--help] [-v|--version] [-i|--purpose] [-c|--coordinates] "
+        "[-o|--output_directory] [-t|--daily_tables] [-s|--climatology] <-a|--authentication>\n\n"
+        "[-h|--help]\tprint this help page and exit\n"
+        "[-v|--version]\tprint version\n"
+        "[-i|--purpose]\tshow program's purpose\n\n"
+        "[-c|--coordinates]\t...\n"
+        "[-o|--output_directory]\t...\n"
+        "[-t|--daily_tables]\tbuild daily tables? Default: false\n"
+        "[-s|--climatology]\tbuild climatology? Default: false\n\n"
+        "Optional argument:\n"
+        "<-a|--authentication>\toptional...\n");
+}
+
+void print_version(void) {
+    printf("FORCE version: %s\n", FORCE_VERSION);
+}
+
+void print_purpose(void) {
+    printf("Download ECMWF CAMS data from the Atmosphere Data Store\n");
+}
+
+int init_api_authentication(const struct API_AUTHENTICATION *api_authentication, const struct OPTIONS *options) {
+    return 0;
+}
+
+void free_api_authentication(const struct API_AUTHENTICATION *api_authentication) {
+    free(api_authentication->base_url);
+    free(api_authentication->user);
+    free(api_authentication->secret);
+}
+
+const char *reverse_code_optopt(const char *dest, int optopt) {
+    switch (optopt) {
+        case 'h':
+            dest = "help";
+            break;
+        case 'v':
+            dest = "version";
+            break;
+        case 'p':
+            dest = "purpose";
+            break;
+        case 'a':
+            dest = "authentication";
+            break;
+        case 'c':
+            dest = "coordinates";
+            break;
+        case 'o':
+            dest = "output_directory";
+            break;
+        default:
+            exit(129);
+    }
+
+    return dest;
 }
--- src/download.h
+++ src/download.h
@@ -1,14 +1,91 @@
-//
-// Created by florian on 08.02.23.
-//
-
 #ifndef CAMS_DOWNLOAD_H
 #define CAMS_DOWNLOAD_H
 
-struct DOWNLOAD_OPTIONS
-{
-    int a;
+#include <stdbool.h>
+
+#define FORCE_VERSION "Test, Test!"
+
+enum {
+    NPOW2 = 4,
+    NPOW4 = 16,
+    NPOW6 = 64,
+    NPOW8 = 256,
+    NPOW10 = 1024,
+    NPOW12 = 4096,
+    NPOW14 = 16384,
+    NPOW16 = 65536,
+    NPOW18 = 262144,
+    NPOW20 = 1048576,
+    NPOW22 = 4194304,
+    NPOW24 = 16777216,
+    NPOW26 = 67108864,
+    NPOW28 = 268435456,
+    NPOW30 = 1073741824
 };
+
+/**
+ * @brief
+ */
+struct OPTIONS {
+    char authentication[NPOW16];
+    char coordinates[NPOW16];
+    char output_directory[NPOW16];
+};
+
+/**
+ * @struct API_AUTHENTICATION
+ * @brief struct containing all variables/fields defined in the `.cdsapirc` file
+ * @author Florian Katerndahl
+ */
+struct API_AUTHENTICATION {
+    char *base_url;     ///< Base url for API requests
+    char *user;         ///< UID
+    char *secret;       ///< secret used for authentication
+    bool verify;        ///< verify the tls-ssl connection?
+};
+
+/**
+ * @brief Print the usage of the `download-cams` program
+ * @author Florian Katerndahl
+ */
 void print_usage(void);
+
+/**
+ * @brief Print the version of FORCE
+ * @author Florian Katerndahl
+ */
+void print_version(void);
+
+/**
+ * @brief Print the program's purpose
+ * @author Florian Katerndahl
+ */
+void print_purpose(void);
+
+/**
+ * @brief
+ * @param api_authentication
+ * @param options
+ * @return
+ * @author Florian Katerndahl
+ */
+int init_api_authentication(const struct API_AUTHENTICATION *api_authentication, const struct OPTIONS *options);
+
+/**
+ * @brief
+ * @param api_authentication
+ * @author Florian Katerndahl
+ */
+void free_api_authentication(const struct API_AUTHENTICATION *api_authentication);
+
+/**
+ * @brief
+ * @param dest
+ * @param optopt
+ * @return
+ * @warning This could easily get out of sync with the rest of the program. Terminates program with exit code 129 if
+ * no optcode could be matched
+ */
+const char *reverse_code_optopt(const char *dest, int optopt);
 
 #endif //CAMS_DOWNLOAD_H
